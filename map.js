254fb10d7de6a857eab12ded8dd6d682c13fcc00
Gagan Manjunatha.Gowda <gagan.manjunatha_gowda@dfki.de>
2019-01-07 11:47:48 +0100

    updated

--- map.js
+++ map.js
@@ -8,27 +8,27 @@ var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest
        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
        }).addTo(map);
 
-var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-      maxZoom: 17,
-     attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-    }).addTo(map);
+// var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+//       maxZoom: 17,
+//      attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+//     }).addTo(map);
 
-var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-});
+// var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+// 	maxZoom: 18,
+// 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// });
 
 
         var baselayers = {
           "Topographic" : topographic,
-          "GoogleEarth" : Esri_WorldImagery,
-          "TopoMap" : OpenTopoMap,
-          "OpenStreetMap": OpenStreetMap_DE
+          "GoogleEarth" : Esri_WorldImagery
+        //  "TopoMap" : OpenTopoMap,
+         // "OpenStreetMap": OpenStreetMap_DE
         };
 
         
 
 // Shapefile control
-L.control.shapefile({ position: 'topleft' }).addTo(map);
+//L.control.shapefile({ position: 'topleft' }).addTo(map);
 
 L.control.layers(baselayers).addTo(map); 
